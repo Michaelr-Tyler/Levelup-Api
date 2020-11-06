05bfc448673a8301b92c2a19929dc0087c4f56f5
Michael-Tyler <miketylerabc@gmail.com>
2020-11-06 10:56:03 -0600

    added the join and leave logic for the client

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -7,9 +7,43 @@ from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from levelupapi.models import Game, Event, Gamer
+from levelupapi.models import Game, Event, Gamer, EventGamer
 # from levelupapi.views.game import GameSerializer
 
+class EventUserSerializer(serializers.ModelSerializer):
+    """JSON serializer for event organizer's related Django user"""
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class EventGamerSerializer(serializers.ModelSerializer):
+    """JSON serializer for event organizer"""
+    user = EventUserSerializer(many=False)
+
+    class Meta:
+        model = Gamer
+        fields = ['user']
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for games"""
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'number_of_players', 'skill_level')
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for events"""
+    gamer = EventGamerSerializer(many=False)
+    game = GameSerializer(many=False)
+
+    class Meta:
+        model = Event
+        url = serializers.HyperlinkedIdentityField(
+            view_name='event',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'game', 'gamer',
+                  'location', 'day', 'time', 'joined')
 
 class Events(ViewSet):
     """Level up events"""
@@ -95,7 +129,19 @@ class Events(ViewSet):
         Returns:
             Response -- JSON serialized list of events
         """
+        # Get the current authenticated user
+        gamer = Gamer.objects.get(user=request.auth.user)
         events = Event.objects.all()
+
+        # Set the `joined` property on every event
+        for event in events:
+            event.joined = None
+
+            try:
+                EventGamer.objects.get(event=event, gamer=gamer)
+                event.joined = True
+            except EventGamer.DoesNotExist:
+                event.joined = False
 
         # Support filtering events by game
         game = self.request.query_params.get('gameId', None)
@@ -106,38 +152,65 @@ class Events(ViewSet):
             events, many=True, context={'request': request})
         return Response(serializer.data)
 
+    @action(methods=['get', 'post', 'delete'], detail=True)
+    def signup(self, request, pk=None):
+        """Managing gamers signing up for events"""
 
-class EventUserSerializer(serializers.ModelSerializer):
-    """JSON serializer for event organizer's related Django user"""
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email']
+        # A gamer wants to sign up for an event
+        if request.method == "POST":
+            # The pk would be `2` if the URL above was requested
+            event = Event.objects.get(pk=pk)
 
+            # Django uses the `Authorization` header to determine
+            # which user is making the request to sign up
+            gamer = Gamer.objects.get(user=request.auth.user)
 
-class EventGamerSerializer(serializers.ModelSerializer):
-    """JSON serializer for event organizer"""
-    user = EventUserSerializer(many=False)
+            try:
+                # Determine if the user is already signed up
+                registration = EventGamer.objects.get(
+                    event=event, gamer=gamer)
+                return Response(
+                    {'message': 'Gamer already signed up this event.'},
+                    status=status.HTTP_422_UNPROCESSABLE_ENTITY
+                )
+            except EventGamer.DoesNotExist:
+                # The user is not signed up.
+                registration = EventGamer()
+                registration.event = event
+                registration.gamer = gamer
+                registration.save()
 
-    class Meta:
-        model = Gamer
-        fields = ['user']
+                return Response({}, status=status.HTTP_201_CREATED)
 
-class GameSerializer(serializers.HyperlinkedModelSerializer):
-    """JSON serializer for games"""
-    class Meta:
-        model = Game
-        fields = ('id', 'title', 'number_of_players', 'skill_level')
+        # User wants to leave a previously joined event
+        elif request.method == "DELETE":
+            # Handle the case if the client specifies a game
+            # that doesn't exist
+            try:
+                event = Event.objects.get(pk=pk)
+            except Event.DoesNotExist:
+                return Response(
+                    {'message': 'Event does not exist.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
 
-class EventSerializer(serializers.HyperlinkedModelSerializer):
-    """JSON serializer for events"""
-    gamer = EventGamerSerializer(many=False)
-    game = GameSerializer(many=False)
+            # Get the authenticated user
+            gamer = Gamer.objects.get(user=request.auth.user)
 
-    class Meta:
-        model = Event
-        url = serializers.HyperlinkedIdentityField(
-            view_name='event',
-            lookup_field='id'
-        )
-        fields = ('id', 'url', 'game', 'gamer',
-                  'location', 'day', 'time')
+            try:
+                # Try to delete the signup
+                registration = EventGamer.objects.get(
+                    event=event, gamer=gamer)
+                registration.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+            except EventGamer.DoesNotExist:
+                return Response(
+                    {'message': 'Not currently registered for event.'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+        # If the client performs a request with a method of
+        # anything other than POST or DELETE, tell client that
+        # the method is not supported
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
